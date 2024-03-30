92b053fb4348fb837af297c7c0f52e7cc9397a35
ilambrev <ilambrev@abv.bg>
2024-03-30 21:24:26 +0200

    Extract The Domain Name From A URL

---
+++ JavaScript_Solutions/007ExtractTheDomainNameFromAURL.js
@@ -0,0 +1,25 @@
+function domainName(url) {
+
+    let domainName = url;
+
+    if (domainName.includes('www')) {
+        domainName = domainName.substring(indexOfElement(domainName, '.') + 1);
+    } else if (domainName.includes('//')) {
+        domainName = domainName.substring(indexOfElement(domainName, '//') + 2);
+    }
+
+    domainName = domainName.substring(0, indexOfElement(domainName, '.'));
+
+    function indexOfElement(domainName, element) {
+        return domainName.indexOf(element);
+    }
+
+    return domainName;
+
+}
+
+domainName("http://github.com/carbonfive/raygun");
+domainName("http://www.zombie-bites.com");
+domainName("https://www.cnet.com");
+domainName("84pvs.name/warez/");
+domainName("zdhst99g9i2o-5i.it/archive/");
