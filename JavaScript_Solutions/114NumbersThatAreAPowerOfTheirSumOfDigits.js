67f3329eaeb7ed5a64a3e812385ff8aaee747263
ilambrev <ilambrev@abv.bg>
2024-12-30 21:08:55 +0200

    Numbers That Are A Power Of Their Sum Of Digits

---
+++ JavaScript_Solutions/114NumbersThatAreAPowerOfTheirSumOfDigits.js
@@ -0,0 +1,30 @@
+function powerSumDigTerm(n) {
+    const nums = [];
+
+    for (let i = 2; i <= 100; i++) {
+        let result = i;
+        for (let j = 2; j <= 10; j++) {
+            result = result * i;
+            let digitsSum = result.toString()
+            .split('')
+            .reduce((acc, curr) => acc + Number(curr), 0);
+
+            if (digitsSum == i) {
+                nums.push(result);
+            }
+        }
+    }
+
+    nums.sort((n1, n2) => n1 - n2);
+
+    return nums[n - 1];
+}
+
+console.log(powerSumDigTerm(1));
+console.log(powerSumDigTerm(2));
+console.log(powerSumDigTerm(3));
+console.log(powerSumDigTerm(4));
+console.log(powerSumDigTerm(5));
+console.log(powerSumDigTerm(6));
+console.log(powerSumDigTerm(7));
+console.log(powerSumDigTerm(8));
