a916cecc87e4414d676ce6fa0a4c734fcd164f1f
Ivan Lambrev <ilambrev@abv.bg>
2025-07-15 10:41:28 +0300

    Count The Monkeys!

---
+++ Python_Solutions/011CountTheMonkeys.py
@@ -0,0 +1,8 @@
+def monkey_count(n):
+    return [n for n in range(1, n + 1)]
+
+# print(monkey_count(5))
+# print(monkey_count(3))
+# print(monkey_count(9))
+# print(monkey_count(10))
+# print(monkey_count(20))
