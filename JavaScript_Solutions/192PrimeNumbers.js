34040786ed0aa25a311cf8c57e882ff40defd9d0
Ivan Lambrev <ilambrev@abv.bg>
2025-09-04 12:32:54 +0300

    Prime Numbers

--- JavaScript_Solutions/192PrimeNumbers.js
+++ JavaScript_Solutions/192PrimeNumbers.js
@@ -30,13 +30,13 @@ function getPrimes(start, finish) {
     return primeNumbers;
 }
 
-// console.log(isPrime(0)); // false
-// console.log(isPrime(1)); // false
-// console.log(isPrime(2)); // true
-// console.log(isPrime(3)); // true
-// console.log(isPrime(4)); // false
-// console.log(isPrime(5)); // true
+// console.log(isPrime(0));
+// console.log(isPrime(1));
+// console.log(isPrime(2));
+// console.log(isPrime(3));
+// console.log(isPrime(4));
+// console.log(isPrime(5));
 
-// console.log(getPrimes(0, 0)); // []
-// console.log(getPrimes(0, 30)); // [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
-// console.log(getPrimes(30, 0)); // [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+// console.log(getPrimes(0, 0));
+// console.log(getPrimes(0, 30));
+// console.log(getPrimes(30, 0));
