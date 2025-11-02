fa68cb81c73fe1c90bae3308d9e031c7726a2994
Ivan Lambrev <ilambrev@gmail.com>
2025-11-02 09:01:04 +0200

    Feynman's Square Question

---
+++ Python_Solutions/056_feynmans_square_question.py
@@ -0,0 +1,9 @@
+def count_squares(n):
+    return sum([num ** 2 for num in range(n + 1)])
+
+# print(count_squares(1))
+# print(count_squares(2))
+# print(count_squares(3))
+# print(count_squares(5))
+# print(count_squares(8))
+# print(count_squares(15))
