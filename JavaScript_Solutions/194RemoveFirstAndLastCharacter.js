92daf038811bfd83f504a9a1a8abd54215677a3e
Ivan Lambrev <ilambrev@gmail.com>
2026-09-14 13:58:23 +0300

    Remove First And Last Character

---
+++ JavaScript_Solutions/194RemoveFirstAndLastCharacter.js
@@ -0,0 +1,11 @@
+function removeChar(str) {
+    return str.slice(1, str.length - 1);
+}
+
+// console.log(removeChar('eloquent'));
+// console.log(removeChar('country'));
+// console.log(removeChar('person'));
+// console.log(removeChar('place'));
+// console.log(removeChar('ooopsss'));
+// console.log(removeChar('ab'));
+// console.log(removeChar('xyz'));
